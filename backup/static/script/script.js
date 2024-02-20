30808136039802c918c5648a567f3d0ad55b31f5
Nickolay-Yesipenko <nickolyacipe@gmail.com>
2024-02-20 21:22:08 +0300

    20.02.2024
    21.22

---
+++ backup/static/script/script.js
@@ -0,0 +1,17 @@
+let main_block = document.querySelector('#width');
+
+let button1 = document.querySelector('#button1');
+
+let button2 = document.querySelector('#button2');
+
+/*button1.addEventListener('click', function() {
+	main_block.class = 'width200';
+});
+
+button2.addEventListener('click', function() {
+	main_block.class = 'width400';
+});*/
+
+console.log(document.querySelector('#button1').width);
+
+console.log(document.querySelector('#button2').width);
